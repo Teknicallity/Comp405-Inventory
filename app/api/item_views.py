e47d0f427b5939e6583c41018cf4efe085ffa3ee
Teknicallity <joshua.s.3322@gmail.com>
2024-11-09 10:04:43 -0500

    Add item crud api endpoints

--- app/api/item_views.py
+++ app/api/item_views.py
@@ -1,13 +1,16 @@
-from flask import jsonify, request
+from flask import jsonify, request, render_template
 
+from db.models.item_model import ItemModel, add_item
 from . import api
 from db.models import item_model
+from .forms import CreateItemForm, UpdateItemForm, DeleteItemForm
+from .. import debug_only
 
 
-@api.route('/items')
+@api.route('/items/')
 def all_items():
     items = item_model.get_all_items()
-    return jsonify(items.to_dict()), 200
+    return jsonify(list(map((lambda item: item.to_dict()), items))), 200
 
 
 @api.route('/items/<int:item_id>', methods=['GET'])
@@ -19,24 +22,67 @@ def get_single_item(item_id):
         return jsonify({"error": "Item not found"}), 404
 
 
+@api.route('/items/', methods=['POST'])
+def create_item():
+    data = request.get_json()
+
+    name = data.get('name')
+    if not name:
+        return jsonify({"error": "The 'name' field is required"}), 400
+
+    brand = data.get('brand') or None
+    model = data.get('model') or None
+    serial_number = data.get('serial_number') or None
+
+    item = ItemModel(name, brand, model, serial_number)
+    new_item = add_item(item)
+
+    return jsonify(new_item.to_dict()), 201
+
+
 @api.route('/items/<int:item_id>', methods=['PUT'])
 def update_item(item_id):
     item = item_model.get_item_by_id(item_id)
     if not item:
-        return jsonify({"error": "Item not found"}), 404
+        return jsonify({"error": f"Item with ID {item_id} not found"}), 404
 
     data = request.get_json()
+    fields_to_update = {
+        "name": data.get("name"),
+        "brand": data.get("brand"),
+        "model_number": data.get("model"),
+        "serial_number": data.get("serial_number"),
+    }
 
-    name = data.get('name', item.name)
-    brand = data.get('brand', item.brand)
-    model_number = data.get('model_number', item.model_number)
-    serial_number = data.get('serial_number', item.serial_number)
-
-    item.name = name
-    item.brand = brand
-    item.model_number = model_number
-    item.serial_number = serial_number
+    for field, value in fields_to_update.items():
+        if value not in (None, ""):
+            setattr(item, field, value)
 
     item_model.update_item(item)
 
     return jsonify(item.to_dict()), 200
+
+
+@api.route('/items/<int:item_id>', methods=['DELETE'])
+def delete_item(item_id):
+    item = item_model.get_item_by_id(item_id)
+    if not item:
+        return jsonify({"error": "Item not found"}), 404
+
+    item_model.delete_item(item.item_id)
+    return jsonify(item.to_dict()), 200
+
+
+@api.route('/itemtest/', methods=['GET'])
+@debug_only
+def all_test():
+    create_form = CreateItemForm()
+    update_form = UpdateItemForm()
+    delete_form = DeleteItemForm()
+
+    return render_template(
+        'example_requests.html',
+        create_form=create_form,
+        update_form=update_form,
+        delete_form=delete_form
+    )
