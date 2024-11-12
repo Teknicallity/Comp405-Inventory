5f0d52999d774cb599ff62c919a1087520e84097
Teknicallity <joshua.s.3322@gmail.com>
2024-11-12 09:36:22 -0500

    Add test print qrcode page

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,7 @@
 from flask_login import login_required, current_user
 from flask import jsonify, render_template, abort
 
+from app import debug_only
 from app.main import main
 from db.models.user_model import User
 
@@ -8,3 +9,9 @@ from db.models.user_model import User
 @main.route('/')
 def index():
     return render_template('home.html')
+
+
+@main.route('/test')
+@debug_only
+def test():
+    return render_template('test.html')
