f51e8c817f9a734666e3f6ba881fd49e275fe4d1
Teknicallity <joshua.s.3322@gmail.com>
2024-11-13 20:54:37 -0500

    Improve status and location models

--- db/models/location_model.py
+++ db/models/location_model.py
@@ -1,18 +1,41 @@
 from db.connection import get_db
 
 
+class LocationModel:
+    def __init__(self, location_id, location_name):
+        self.location_id = location_id
+        self.location_name = location_name
+
+    @classmethod
+    def from_row(cls, row):
+        return cls(
+            location_id=row[0],
+            location_name=row[1],
+        ) if row else None
+
+    @classmethod
+    def list_from_rows(cls, rows):
+        return [cls.from_row(row) for row in rows]
+
+    def to_dict(self):
+        return {
+            'location_id': self.location_id,
+            'location_name': self.location_name
+        }
+
+
 def get_all_locations():
     db = get_db()
     with db.cursor() as cursor:
-        cursor.execute(f'SELECT * FROM locations')
-        return cursor.fetchall()
+        cursor.execute(f'SELECT location_id, name FROM locations')
+        return LocationModel.list_from_rows(cursor.fetchall())
 
 
 def get_location_by_id(location_id: int):
     db = get_db()
     with db.cursor() as cursor:
-        cursor.execute(f'SELECT * FROM locations WHERE location_id={location_id}')
-        return cursor.fetchone()[1]
+        cursor.execute('SELECT location_id, name FROM locations WHERE location_id = ?', (location_id,))
+        return LocationModel.from_row(cursor.fetchone())
 
 
 def add_location(location_name):
--- db/models/status_model.py
+++ db/models/status_model.py
@@ -1,8 +1,48 @@
 from db.connection import get_db
 
 
+class StatusModel:
+    def __init__(self, status_id, status_name):
+        self.status_id = status_id
+        self.status_name = status_name
+
+    @classmethod
+    def from_row(cls, row):
+        return cls(
+            status_id=row[0],
+            status_name=row[1],
+        ) if row else None
+
+    @classmethod
+    def list_from_rows(cls, rows):
+        return [cls.from_row(row) for row in rows]
+
+    def to_dict(self):
+        return {
+            'status_id': self.status_id,
+            'status_name': self.status_name
+        }
+
+
 def get_all_statuses():
     db = get_db()
     with db.cursor() as cursor:
-        cursor.execute('SELECT * FROM statuses')
-        return cursor.fetchall()
+        cursor.execute('SELECT status_id, name FROM statuses')
+        return StatusModel.list_from_rows(cursor.fetchall())
+
+
+def get_status_by_id(status_id: int):
+    db = get_db()
+    with db.cursor() as cursor:
+        cursor.execute('SELECT status_id, name FROM statuses WHERE status_id = ?', (status_id,))
+        return StatusModel.from_row(cursor.fetchone())
+
+
+def add_status(status_name):
+    db = get_db()
+    with db.cursor() as cursor:
+        cursor.execute(
+            'INSERT INTO statuses (name) VALUES (?)',
+            (status_name,)
+        )
+    db.commit()
